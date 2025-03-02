c3f3256481b807d3424c0eaf34c144ad70f85341
ming-yy <mingeraneos@gmail.com>
2025-03-02 19:30:18 +0100

    02/03/2025 inicio de merge bien hecho

--- game.cpp
+++ game.cpp
@@ -1,3 +1,74 @@
 #include <iostream>
 #include "game.h"
 #include "resources.h"
+
+
+void gestionarEventos() {
+    while (const std::optional<sf::Event> event = window.pollEvent())
+    {
+        if (event->is<sf::Event::Closed>())
+        {
+            window.close();
+        }
+        else if (const auto* keyPressed = event->getIf<sf::Event::KeyPressed>())
+        {
+            switch (keyPressed->scancode)
+            {
+                case sf::Keyboard::Scancode::Escape:
+                    window.close();
+                    break;
+                case sf::Keyboard::Scancode::Z:
+                    ataque = true;
+                    std::cout << "Ataque activado" << std::endl;
+                    break;
+                case sf::Keyboard::Scancode::Up:
+                    haciaArriba = true;
+                    if (isOnGround)
+                    {
+                        verticalSpeed = -JUMP_FORCE;
+                        isOnGround = false;
+                    }
+                    break;
+                case sf::Keyboard::Scancode::Down:
+                    verticalSpeed = JUMP_FORCE * 0.5f;
+                    break;
+                case sf::Keyboard::Scancode::Left:
+                    haciaIzquierda = true;
+                    haciaDerecha = false;
+                    gSimonSprite->setScale({1.f, 1.f});
+                    break;
+                case sf::Keyboard::Scancode::Right:
+                    haciaDerecha = true;
+                    haciaIzquierda = false;
+                    gSimonSprite->setScale({-1.f, 1.f});
+                    break;
+                default:
+                    break;
+            }
+        }
+        else if (const auto* keyReleased = event->getIf<sf::Event::KeyReleased>())
+        {
+            switch (keyReleased->scancode)
+            {
+                case sf::Keyboard::Scancode::Up:
+                    haciaArriba = false;
+                    break;
+                case sf::Keyboard::Scancode::Left:
+                    haciaIzquierda = false;
+                    break;
+                case sf::Keyboard::Scancode::Right:
+                    haciaDerecha = false;
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
+
+
+
+void Update(sf::RenderWindow &window, float deltaTime)
+{
+    gestionarEventos(window);
+}
--- main.cpp
+++ main.cpp
@@ -1,8 +1,69 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
-#include <string>
 #include <iostream>
-#include <sstream>
+#include <iomanip>
+#include <string>
 #include "game.h"
 #include "camera.h"
 #include "resources.h"
+
+
+// Global configuration variables
+constexpr int scale { 1 };
+constexpr int gWindowWidth { 768 * scale };
+constexpr int gWindowHeight { 250 * scale };
+
+
+std::string formatFPSandTime(float deltaTime)
+{
+    std::ostringstream oss;
+    oss << std::fixed << std::setprecision(2) << (1.f / deltaTime);
+    std::string fps = oss.str();
+
+    // Clean the stream
+    oss.str("");
+    oss.clear();
+
+    oss << std::fixed << std::setprecision(2) << (deltaTime * 1000);
+    std::string ms = oss.str();
+    return fps + " FPS\n" + ms + " ms";
+}
+
+int main()
+{
+    std::string windowName { "Castlevania NES" };
+    sf::RenderWindow window(sf::VideoMode({gWindowWidth, gWindowHeight}), windowName, sf::Style::Default);
+    window.setVerticalSyncEnabled(true);
+    sf::Clock deltaClock;
+
+    if (!Init())
+    {
+        std::cerr << "Error en la inicialización" << std::endl;
+        return -1;
+    }
+
+    sf::Font font;
+    if (!font.openFromFile("../assets/fonts/NESfonts/nintendo-nes-font.ttf"))
+    {
+        std::cerr << "Error en la carga de la fuente de texto" << std::endl;
+        return -1;
+    }
+    
+    sf::Text text(font);
+    text.setString("60 FPS\n16.67 ms");
+    text.setCharacterSize(12);
+    text.setFillColor(sf::Color::White);
+    
+    // Game loop
+    while (window.isOpen())
+    {
+        float deltaTime = deltaClock.restart().asSeconds();
+        text.setString(formatFPSandTime(deltaTime));
+
+        Update(window, deltaTime);
+
+        Render(window, text);
+    }
+
+    return 0;
+}
