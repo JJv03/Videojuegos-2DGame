fd5224bfcf61565421518b76242a01d6a535e172
Jorge Leris <845647@unizar.es>
2025-03-02 18:46:51 +0100

    limpia

--- resources.cpp
+++
@@ -1,7 +0,0 @@
-#include "resources.h"
-
-// std::unordered_map<std::string, sf::Texture> textures{};
-
-// std::unordered_map<std::string, sf::Sprite> sprites{};
-
-// std::unordered_map<std::string, sf::RectangleShape> rectangles{};
