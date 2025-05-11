6d25bc77a1545bf788b22c57198b1c3c78cb3b45
ming-yy <mingeraneos@gmail.com>
2025-05-11 23:13:33 +0200

    bugs arreglados

--- globals.cpp
+++ globals.cpp
@@ -28,7 +28,7 @@ const sf::Color gColorKeyGreen = sf::Color(0, 128, 0);
 
 // ============ TILEMAP VARIABLES =============
 int gStartingLevel = 3;
-int gStartingStage = 5;
+int gStartingStage = 1;
 
 const float gTileSize = 32.f;
 
@@ -86,7 +86,7 @@ const float gSimonFeetCollisionNewHeight = 0.15f;
 const float gPlayerMovementSpeed = 60.0f;
 const float gPlayerStairSpeed = 30.0f;
 const float gPlayerGravity = 450.0f;
-const float gPlayerJumpForce = 180.0f;
+const float gPlayerJumpForce = 190.0f;
 
 bool gIsWhipBeingUsed = false;
 bool gIsSubWeaponBeingUsed = false;
